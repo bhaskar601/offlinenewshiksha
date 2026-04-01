6abcb5ea61312e4ceb1785b5536c36f71fff76ad
indreshverma2004 <indreshkverma2004@gmail.com>
2026-04-01 13:39:21 +0530

    updated code

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // Cloud sync target (manual sync). Replace with your AWS public IP/domain.
 // Example: export const SERVER_URL = "http://13.234.xxx.xxx:5000";
-export const SERVER_URL = "http://192.168.1.3:5002";
+export const SERVER_URL = "http://13.127.236.196:5002";
 
